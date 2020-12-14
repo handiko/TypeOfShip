0f5fd0f47ec3bfc47470ff226c28a665960e19b3
handiko <handikogesang@gmail.com>
2020-12-14 13:10:31 +0700

    first commit

---
+++ Inc/TypeOfShip.h
@@ -0,0 +1,42 @@
+/*
+ * TypeOfShip.h
+ *
+ *  Created on: Dec 14, 2020
+ *      Author: handiko
+ */
+
+#ifndef INC_TYPEOFSHIP_H_
+#define INC_TYPEOFSHIP_H_
+
+#define PILOT_VESSEL 50
+#define SEARCH_AND_RESCUE 51
+#define TUGS 52
+#define PORT_TENDERS 53
+#define ANTI_POLLUTION_VESSEL 54
+#define LAW_ENFORCEMENT 55
+#define SPARE_1 56
+#define SPARE_2 57
+#define MEDICAL_TRANSPORT 58
+#define SHIPS_AND_AIRCRAFT_OF_STATES_NO_PARTIES 59
+
+#define RESERVED_1 10
+#define HAZARD_CAT_X 21
+#define HAZARD_CAT_Y 32
+#define HAZARD_CAT_Z 43
+#define HAZARD_CAT_OS 54
+#define RESERVED_2 55
+#define PASSENGER 60
+#define CARGO 70
+#define TANKER 80
+#define OTHERS 90
+
+#define FISHING 30
+#define TOWING 31
+#define TOWING_LONG 32
+#define UNDERWATER_OPS 33
+#define DIVING_OPS 34
+#define MILITARY_OPS 35
+#define SAILING 36
+#define PLEASURE 37
+
+#endif /* INC_TYPEOFSHIP_H_ */
